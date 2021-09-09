6eca4d9d64c1f32704435fb1136f70368d79d94a
alinen <alinen@savvysine.com>
2021-09-08 20:47:03 -0400

    remove uneeded motions

--- motions/SignLanguage-2020/smoothAllTrajectories.m
+++
@@ -1,7 +0,0 @@
-% To run this file, first set the workspace directory to 
-%     <path>/motions/SignLanguage-2020
-
-smoothFactor = 2; % smaller -> more detail; larger -> less
-smoothSplineTrajectories(smoothFactor, ...
-   'p01/p01_INTRODUCTION/INTRODUCTION.mat', ...
-   'p01/p01_INTRODUCTION/INTRODUCTION_Smooth.txt');
