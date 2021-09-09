97c2bcc2a7c0f82c0ae604cc0ac18a29c512a750
Trang Dang <huyentrangdang160801@gmail.com>
2021-09-09 14:50:47 -0400

    working eyes

--- assignments/a1-hello/eyes.cpp
+++ assignments/a1-hello/eyes.cpp
@@ -1,14 +1,38 @@
 #include "atkui/framework.h"
 
 using namespace glm;
+using glm::vec3;
+
+
 class Eyes : public atkui::Framework
 {
   public:
     Eyes() : atkui::Framework(atkui::Orthographic) {
+      theta = 0.0f;
+      thetaRate = 0.50f;
+      radius = 30.0f;
     }
 
     virtual void scene() {
+
+      // eyes
+      setColor(vec3(1, 1, 1));
+      drawSphere(vec3(175, 250, 0), 80);
+      drawSphere(vec3(325, 250, 0), 80);
+
+      theta += thetaRate * dt();
+      float px = radius * cos(theta);  
+
+      setColor(vec3(0, 0, 0));
+      drawSphere(vec3(175 + px, 250, 50), 30);
+      drawSphere(vec3(325 + px, 250, 50), 30);
     }
+
+    // eyes moving to the side
+    private:
+      float theta;
+      float thetaRate;
+      float radius; // how wide the moving eyes would go
 };
 
 int main(int argc, char** argv) {
