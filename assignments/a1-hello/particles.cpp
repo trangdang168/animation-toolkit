72d6c90d11060c4c8a2be5ef33b1305d5378612f
Trang Dang <huyentrangdang160801@gmail.com>
2021-09-17 11:21:58 -0400

    finish random particles!

--- assignments/a1-hello/particles.cpp
+++ assignments/a1-hello/particles.cpp
@@ -2,6 +2,18 @@
 
 using namespace glm;
 
+class Particle {
+ public:
+    float speed;
+    vec3 color;
+    vec3 position;
+  Particle() {
+    position = vec3(rand() % 500, rand() % 500, 0);
+    color = agl::randomUnitVector();
+    speed = 100.0f + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(200.0f-100.0f)));
+  } 
+};
+
 class Particles : public atkui::Framework
 {
  public:
@@ -9,13 +21,62 @@ class Particles : public atkui::Framework
   }
 
   virtual void setup() {
+
+    radius = 10.0f;
+    // direction = agl::randomUnitVector();
+    direction = vec3(1, 1, 0);
   }
 
   virtual void scene() {
+
+    for (int i = 0; i < numParticles; i++) {
+      Particle p = particles[i];
+
+      vec3 newPosition = moveParticle(p);
+      setColor(p.color);
+      drawSphere(newPosition, radius);
+    }
   }
+
+  float wrapAround(float num) {
+    int numInt = static_cast<int>(num);
+    int mod = numInt % 500;
+    float intAndFloatDiff = num - static_cast<float>(numInt);
+    float modFloat = static_cast<float>  (mod);
+    return modFloat + intAndFloatDiff;
+  } 
+
+  // move one particle
+  vec3 moveParticle(Particle p) {
+    vec3 newPosition = p.position + p.speed * direction * elapsedTime();
+
+    // wrap arround
+    if (newPosition[0] > 500 || newPosition[0] < 0) {
+      newPosition[0] = wrapAround(newPosition[0]);
+    } 
+
+    if (newPosition[1] > 500 || newPosition[1] < 0) {
+      newPosition[1] = wrapAround(newPosition[1]);
+    } 
+
+    if (newPosition[2] > 500 || newPosition[2] < 0) {
+      newPosition[2] = wrapAround(newPosition[2]);
+    } 
+
+    return newPosition;
+  }
+
+  private:
+    int numParticles = 200;
+    float radius;
+    Particle particles[200];
+    vec3 direction;
 };
 
 int main(int argc, char** argv) {
+
+  // set the seed for randomization
+  srand (static_cast <unsigned> (time(0)));
   Particles viewer;
   viewer.run();
   return 0;
