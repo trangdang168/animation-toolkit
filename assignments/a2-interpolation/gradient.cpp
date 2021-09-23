40dac6f1560de326fd19af9d3da9da78d7d353d1
Trang Dang <huyentrangdang160801@gmail.com>
2021-09-23 19:53:07 -0400

    finish gradient

--- assignments/a2-interpolation/gradient.cpp
+++ assignments/a2-interpolation/gradient.cpp
@@ -5,9 +5,37 @@ using namespace glm;
 class Gradient : public atkui::Framework {
  public:
   Gradient() : atkui::Framework(atkui::Orthographic) {
+  
   }
 
   virtual void scene() {
+
+    vec3 nw = vec3(1,1,0);
+    vec3 ne = vec3(0,1,1);
+    vec3 sw = vec3(1,0,0);
+    vec3 se = vec3(1,0,1);
+
+    int width = 250;
+    int numSquares = 10;
+    int size = width / numSquares;
+
+    int xStart = 125;
+    int yStart = 375;
+
+    for (int i =0; i < numSquares; i++) {
+      for (int j = 0; j < numSquares; j++) {
+        int x = xStart + i * size;
+        int y = yStart - j * size;
+
+        float tx = static_cast <float> (i) /  static_cast <float> (numSquares);
+        float ty = static_cast <float> (j) /  static_cast <float> (numSquares);
+        vec3 colorx0 = nw * (1.0f - tx) + ne * tx;
+        vec3 colorx1 = sw * (1.0f - tx) + se * tx;
+        vec3 color = colorx0 * (1.0f - ty) + colorx1 * (ty);
+        setColor(color);
+        drawCube(vec3(x, y, 0), vec3(size, size, 0));
+      }
+    }
   }
 };
 
