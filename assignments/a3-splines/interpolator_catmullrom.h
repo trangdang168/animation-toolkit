a37155e59a415096e8584fc51587d476ebe2a3f1
Trang Dang <huyentrangdang160801@gmail.com>
2021-10-01 14:51:11 -0400

    finish part 2

--- assignments/a3-splines/interpolator_catmullrom.h
+++ assignments/a3-splines/interpolator_catmullrom.h
@@ -15,14 +15,30 @@ public:
     InterpolatorCatmullRom() : Interpolator("Catmull-Rom") {}
 
     virtual glm::vec3 interpolate(int segment, double u) const {
-       // todo: your code here
-       // see: interpolator.h and writeup for details
-       return glm::vec3(0);
+       float coefficients [4] = {1.0f, 3.0f, 3.0f, 1.0f};
+      //  float uFloat = float(u);
+       glm::vec3 result = glm::vec3(0);
+       for (int i = 0; i < 4; i ++) {
+          float a = float(pow(1.0 - u, 3.0 - i)); // a is 1 - t
+          float b = float(pow(u, i)); // b is t
+          result += coefficients[i] * a * b * mCtrlPoints[i + segment*3];
+       }
+       return result;
     }
 
     virtual void computeControlPoints(const std::vector<glm::vec3>& keys) {
        // todo: your code here
        // see: interpolator.h and writeup for details
+       // segment 0
+       mCtrlPoints.push_back(keys.at(0));
+       mCtrlPoints.push_back(mCtrlPoints.at(0) + (1.0f/6.0f) * (keys.at(1) - keys.at(0)));
+       mCtrlPoints.push_back(keys.at(2) - (1.0f/6.0f) * (keys.at(2) - keys.at(0)));
+
+      // segment 1 keys.at(2) - keys.at(0)
+       mCtrlPoints.push_back(keys.at(1));
+       mCtrlPoints.push_back(keys.at(1) + (1.0f/6.0f) * (keys.at(2) - keys.at(0)));
+       mCtrlPoints.push_back(keys.at(2)- (1.0f/6.0f) * (keys.at(2) - keys.at(1)));
+       mCtrlPoints.push_back(keys.at(2));
     }
 };
 
