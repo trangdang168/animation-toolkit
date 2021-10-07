cafba7d4ca67a427c4d15c34e6c055157b236cbf
Trang Dang <huyentrangdang160801@gmail.com>
2021-10-06 22:38:30 -0400

    finish rotation method

--- libsrc/atkmath/matrix3-basecode.cpp
+++ libsrc/atkmath/matrix3-basecode.cpp
@@ -3,6 +3,46 @@
 
 namespace atkmath {
 
+Matrix3 Rx(float angle) {
+
+   Matrix3 rotationMat = Matrix3(1.0,  0.0,  0.0,
+                                 0.0,  cos(angle),  -sin(angle),
+                                 0.0,  sin(angle),  cos(angle)
+                                 );
+   return rotationMat;
+}
+
+Matrix3 Ry(float angle) {
+
+   Matrix3 rotationMat = Matrix3(cos(angle), 0.0, sin(angle),
+                                 0.0, 1.0, 0.0,
+                                 -sin(angle), 0.0,cos(angle)
+                                 );
+
+   // std::cout << "Ry, sin,cos,angle" << std::endl;
+   // std::cout << rotationMat << std::endl;
+   // std::cout<< sin(angle) <<std::endl;
+   // std::cout<< cos(angle) <<std::endl;
+   // std::cout << angle << std::endl;
+
+   return rotationMat;
+}
+
+Matrix3 Rz(float angle) {
+
+   Matrix3 rotationMat = Matrix3(cos(angle),  -sin(angle),  0.0,
+                                 sin(angle),  cos(angle),  0.0,
+                                 0.0,  0.0,  1.0
+                                 );
+   // std::cout << "Rz, sin,cos,angle" << std::endl;
+   // std::cout << rotationMat << std::endl;
+   // std::cout<< sin(angle) <<std::endl;
+   // std::cout<< cos(angle) <<std::endl;
+   // std::cout << angle << std::endl;
+
+   return rotationMat;
+}
+
 Vector3 Matrix3::toEulerAnglesXYZ() const
 {
    return Vector3();
@@ -36,11 +76,20 @@ Vector3 Matrix3::toEulerAnglesZYX() const
 void Matrix3::fromEulerAnglesXYZ(const Vector3& angleRad)
 {
    *this = Identity;
+
+   // 
 }
 
 void Matrix3::fromEulerAnglesXZY(const Vector3& angleRad)
 {
    *this = Identity;
+   Matrix3 result = Rx(angleRad[0]) * Rz(angleRad[2]) * Ry(angleRad[1]);
+
+   *this = result;
+
+   std::cout << "angle rad" <<std::endl;
+   std::cout << angleRad <<std::endl;
+
 }
 
 void Matrix3::fromEulerAnglesYXZ(const Vector3& angleRad)
@@ -61,6 +110,7 @@ void Matrix3::fromEulerAnglesZXY(const Vector3& angleRad)
 void Matrix3::fromEulerAnglesZYX(const Vector3& angleRad)
 {
    *this = Identity;
+   Matrix3 result = Rz(angleRad[2]) * Ry(angleRad[1]) * Rx(angleRad[0]);
 }
 
 void Matrix3::toAxisAngle(Vector3& axis, double& angleRad) const
